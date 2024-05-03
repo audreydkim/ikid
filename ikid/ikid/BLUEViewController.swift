04ef244405f1fd974cea8f46c9eb72054828311b
audreydk <audreydk@cs.washington.edu>
2024-05-02 23:25:34 -0700

    added extra joke

--- ikid/ikid/BLUEViewController.swift
+++ ikid/ikid/BLUEViewController.swift
@@ -16,10 +16,13 @@ class BLUEViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     var counter : Int = 0
-    let nocknock : [String] = ["Nock nock.", "Who's there?", "Yo-deh-lay-hee.", "Yo-deh-lay-hee-WHO!"]
+    let nocknock : [String] = ["Nock nock.", "Who's there?", "Yo-deh-lay-hee.", "Yo-deh-lay-hee-WHO?"]
 
+    let lobster : [String] = ["What’s the difference between a dirty bus stop and a lobster with breast implants?", "One is a crusty bus station, and the other is a busty crustacean!"]
     var jokeCounter : Int = 0
-    /*
+    
+    let allJokes : [[String]] = [["Nock nock.", "Who's there?", "Yo-deh-lay-hee.", "Yo-deh-lay-hee-WHO!"], ["What’s the difference between a dirty bus stop and a lobster with breast implants?", "One is a crusty bus station, and the other is a busty crustacean!"]]
+    /*[[]] [0]
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -28,15 +31,44 @@ class BLUEViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBOutlet weak var button: UIButton!
     
     @IBOutlet weak var joke: UILabel!
     
     @IBAction func next(_ sender: Any) {
-        if (counter >= 3) {
+        if (counter == -1) {
+            button.setTitle("Next", for: .normal)
+        }
+        if (jokeCounter == 0) {
+            // nocknock joke
+            nockJoke()
+        } else {
+            crabJoke()
+        }
+    }
+    func nockJoke() {
+        if (counter < 3) { // if we are still looking at nock nock joke
+            if (counter == 2) {
+                button.setTitle("Next Joke", for: .normal)
+                jokeCounter = 1
+            }
+            counter += 1
+            joke.text = nocknock[counter]
+        }
+    }
+    func crabJoke() {
+        if (counter == 3) {
+            counter = 0
+            button.setTitle("Next", for: .normal)
+            joke.text = lobster[0]
+        } else {
+            counter += 1
+            joke.text = lobster[counter]
+            jokeCounter = 0
+            button.setTitle("Previous Joke", for: .normal)
             counter = -1
         }
-        counter += 1
-        joke.text = nocknock[counter]
+
     }
     
 }
