5fb9dc472bed526dc9425993bfb48fc597da6695
audreydk <audreydk@cs.washington.edu>
2024-05-02 20:53:59 -0700

    establish Stupid joke view controller and functionality

--- ikid/ikid/BLUEViewController.swift
+++ ikid/ikid/BLUEViewController.swift
@@ -12,10 +12,13 @@ class BLUEViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        joke.text = nocknock[0]
         // Do any additional setup after loading the view.
     }
-    
+    var counter : Int = 0
+    let nocknock : [String] = ["Nock nock.", "Who's there?", "Yo-deh-lay-hee.", "Yo-deh-lay-hee-WHO!"]
 
+    var jokeCounter : Int = 0
     /*
     // MARK: - Navigation
 
@@ -25,5 +28,15 @@ class BLUEViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBOutlet weak var joke: UILabel!
+    
+    @IBAction func next(_ sender: Any) {
+        if (counter >= 3) {
+            counter = -1
+        }
+        counter += 1
+        joke.text = nocknock[counter]
+    }
+    
 }
